bf2b49a5930ced76a09465232710d5d84bbbc1e9
Daniel Goldman <dzgoldman@wesleyan.edu>
2016-04-06 10:11:55 -0400

    csv route (jquery), getting rid of json google sheet stuff

--- routes/cvsUpload.js
+++ routes/cvsUpload.js
@@ -1,5 +1,20 @@
   module.exports.controller =  function (app, User) {
 
+    app.post('/upload2', function(req, res) {
+          res.send(req.body)
+          console.log('File size is ' + req.files.groupfile.size);
+          console.log('File size is ' + req.files.groupfile.path);
+          // var reader = csv.createCsvFileReader(req.files.groupfile.path, {
+          //                                         'separator': ',',
+          //                                         'quote': '"',
+          //                                         'escape': '"',
+          //                                         'comment': ''
+          //                                      });
+          // reader.addListener('data', function(data) {
+          //         console.log(data);
+          // });
+    });
+
     app.post('/upload', function (req, res) {
 
       var usersArray = req.body.data;
@@ -7,16 +22,16 @@
       var index =0;
 
       function saveUser(usersArray, index) {
-        var user = usersArray[index];
-        for(key in user){
-          console.log(key, user[key]);
-          if (user[key]) {
+
+        var dbUser = User.build();
+        var csvUser = usersArray[index];
+        for(key in csvUser){
+          console.log(key, csvUser[key]);
+          if (csvUser[key]) {
             console.log('he has a ', key);
           }else{
             console.log('got no', key);
           }
-
-
         }
       }
       saveUser(usersArray, 50)
