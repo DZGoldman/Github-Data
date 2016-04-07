9570078368776d5646d7d5fafb0b87d3a8455a60
Daniel Goldman <dzgoldman@wesleyan.edu>
2016-04-07 13:18:38 -0400

    splitting up browser side uploading function, seems best approach

--- Public/js/app.js
+++ Public/js/app.js
@@ -2,12 +2,21 @@ $(function() {
   console.log('Hello, Dave.');
 
 
-  $('.skills-buttons').click(function (evt) {
+  $('#save-to-db').click(function(evt) {
     var action = this.id
-    upload(action, 'talent')
+    var file = upload('talent');
+    console.log(typeof file);
+    console.log(file);
+    read(file, 'save-to-db')
   });
 
-  $('#compare').click(function (evt) {
+  $('#export-csv').click(function (evt) {
+    var action = this.id
+    var file = upload('talent');
+    read(file, 'export-csv')
+  })
+
+  $('#compare').click(function(evt) {
     var action = this.id
     upload(action, 'talent')
   });
@@ -23,7 +32,8 @@ $(function() {
     }
     return isCompatible;
   };
-  function upload(action, source) {
+
+  function upload(source) {
 
     console.log('uploading');
     if (!browserSupportFileUpload()) {
@@ -31,31 +41,36 @@ $(function() {
     } else {
       $('#results').empty();
       var data = null;
-      if (source=='talent') {
-          var file =$('#userFileUpload')[0];
-      }else if (source=='jobs') {
-        var file =$('#jobsFileUpload')[0];
-      }else{
+      if (source == 'talent') {
+        var file = $('#userFileUpload')[0];
+      } else if (source == 'jobs') {
+        var file = $('#jobsFileUpload')[0];
+      } else {
         return 'something is wrong here...'
       }
       var file = file.files[0];
-      var reader = new FileReader();
-      reader.readAsText(file);
-      reader.onload = function(event) {
-        var csvData = event.target.result;
-        data = $.csv.toObjects(csvData);
-        if (data && data.length > 0) {
-          console.log('Imported -' + data.length + '- rows into the browser successfully!');
-          sendToServer(data,action)
-        } else {
-          alert('No data to import!');
-        };
-      };
-      reader.onerror = function() {
-        alert('Unable to read ' + file.fileName);
-      };
+      return file
     }
   };
 
+  function read(file, action) {
+
+    var reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = function(event) {
+      var csvData = event.target.result;
+      data = $.csv.toObjects(csvData);
+      if (data && data.length > 0) {
+        console.log('Imported -' + data.length + '- rows into the browser successfully!');
+        sendToServer(data, action)
+      } else {
+        alert('No data to import!');
+      };
+    };
+    reader.onerror = function() {
+      alert('Unable to read ' + file.fileName);
+    };
+
+  }
 
 })
