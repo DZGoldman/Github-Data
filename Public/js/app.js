ffd6a8e55bd46de197b28266f4022990750f6e14
Daniel Goldman <dzgoldman@wesleyan.edu>
2016-04-07 14:05:34 -0400

    job uploading and csv uploadingon client side in same scope

--- Public/js/app.js
+++ Public/js/app.js
@@ -1,80 +1,37 @@
 $(function() {
-  console.log('Hello, Dave.');
+  console.log('I am in here.');
 
 
   $('#save-to-db').click(function(evt) {
-
-    var action = this.id
-    var file = upload('talent', afterRead);
-    read(file, 'save-to-db', afterRead);
-    console.log(AJAX);
-    function afterRead(csvData) {
-      data = $.csv.toObjects(csvData);
-
-      if (data && data.length > 0) {
-        sendToServer(data, action)
-
-      } else {
-        alert('No data to import!');
-      };
-
+    var file = upload('talent');
+    read(file, afterRead);
+    function afterRead(data) {
+      AJAX.saveToDb(data)
+      .done(AJAX.doneSaveToDb);
     };
   });
 
-  $('#export-csv').click(function (evt) {
-    var action = this.id
+
+  $('#export-csv').click(function(evt) {
     var file = upload('talent');
-    read(file, 'export-csv')
-  })
+    read(file, afterRead);
+    function afterRead() {
+      AJAX.exportCSV(data, 'save')
+      .done(AJAX.doneExportCSV)
+    }
+  });
 
   $('#compare').click(function(evt) {
-    var action = this.id
-    upload(action, 'talent')
+    var file = upload('jobs');
+    read(file, afterRead);
+    function afterRead(jobData) {
+      var file = upload('talent');
+      read(file, afterRead)
+      function afterRead(talentData) {
+        console.log([jobData, talentData]);
+      }
+    };
   });
 
-
-  // Method that reads and processes the selected file, code (mostly) from here:
-  //https://cmatskas.com/importing-csv-files-using-jquery-and-html5/x
-  //helper function:
-  function browserSupportFileUpload() {
-    var isCompatible = false;
-    if (window.File && window.FileReader && window.FileList && window.Blob) {
-      isCompatible = true;
-    }
-    return isCompatible;
-  };
-
-  function upload(source) {
-
-    console.log('uploading');
-    if (!browserSupportFileUpload()) {
-      alert('The File APIs are not fully supported in this browser!');
-    } else {
-      $('#results').empty();
-      var data = null;
-      if (source == 'talent') {
-        var file = $('#userFileUpload')[0];
-      } else if (source == 'jobs') {
-        var file = $('#jobsFileUpload')[0];
-      } else {
-        return 'something is wrong here...'
-      }
-      var file = file.files[0];
-      return file
-    }
-  };
-
-  function read(file, action, afterRead) {
-
-    var reader = new FileReader();
-    reader.readAsText(file);
-    reader.onload = function(event) {
-      afterRead(event.target.result);
-    };
-    reader.onerror = function() {
-      alert('Unable to read ' + file.fileName);
-    };
-
-  }
 
 })
---
+++ Public/js/csv-upload.js
@@ -0,0 +1,51 @@
+
+
+  // Method that reads and processes the selected file, code (mostly) from here:
+  //https://cmatskas.com/importing-csv-files-using-jquery-and-html5/x
+  //helper function:
+  function browserSupportFileUpload() {
+    var isCompatible = false;
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+      isCompatible = true;
+    }
+    return isCompatible;
+  };
+
+  function upload(source) {
+
+    if (!browserSupportFileUpload()) {
+      alert('The File APIs are not fully supported in this browser!');
+    } else {
+      $('#results').empty();
+      var data = null;
+      if (source == 'talent') {
+        var file = $('#userFileUpload')[0];
+      } else if (source == 'jobs') {
+        var file = $('#jobsFileUpload')[0];
+      } else {
+        return 'something is wrong here...'
+      }
+      var file = file.files[0];
+      return file
+    }
+  };
+
+  function read(file, afterRead) {
+
+    var reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = function(event) {
+      var csvData = event.target.result;
+      data = $.csv.toObjects(csvData);
+
+      if (data && data.length > 0) {
+        afterRead(data)
+      } else {
+        alert('No data to import!');
+      };
+    };
+    reader.onerror = function() {
+      alert('Unable to read ' + file.fileName);
+    };
+
+  }
